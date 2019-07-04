5d82ec467777480ec17a6870db33211c104973e9
sumit <sumit.shairya@gmail.com>
2019-07-04 17:12:54 +0530

    config changes

--- config/constants.js
+++ config/constants.js
@@ -5,13 +5,12 @@ module.exports = {
 	"j_password":'sushant@123',
 	"url":'https://auth.unicommerce.com/login',
 	"uniCommerceProjects":{
-		'9':{'name':'secretwish','mavenLoginDomain':'anssecretwish','enable':true,'pushOrders':true},
-		'4':{'name':'grandpitstop2','mavenLoginDomain':'ansgps','enable':true,'pushOrders':true},
-		'2':{'name':'anscommerce','mavenLoginDomain':'ansdigital','enable':true,'pushOrders':true},
-		'6':{'name':'markmediums','mavenLoginDomain':'anscultfiction','enable':true,'pushOrders':true},
-		'5':{'name':'jeradobrandsprivatelimited','mavenLoginDomain':'ansdisrupt','enable':true,'pushOrders':true}
+		'1':{'name':'anscommerce','mavenLoginDomain':'ansdigital','enable':true,'pushOrders':true},
+		'2':{'name':'grandpitstop2','mavenLoginDomain':'ansgps','enable':true,'pushOrders':true},
+		'3':{'name':'jeradobrandsprivatelimited','mavenLoginDomain':'ansdisrupt','enable':true,'pushOrders':true},
+		'4':{'name':'markmediums','mavenLoginDomain':'anscultfiction','enable':true,'pushOrders':true},
+		'5':{'name':'secretwish','mavenLoginDomain':'anssecretwish','enable':true,'pushOrders':true}
 	},
-	"maven_url": "https://anssecretwish.gscmaven.com",
 	"maven_auth_email": "apiintegration@anssecretwish.com",
 	"maven_auth_password": "abcd@1234",
     "project_name":"express-mvc-generator",
@@ -24,3 +23,5 @@ module.exports = {
 	"alert_email":"sumit.shairya@email.com",
 	"alert_email_name":"MyApp"
 };
+
+
